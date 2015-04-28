bab7056b747c53e41ad17149af8c71b515803c33
Ryo Makabe <penginryo@Ryos-MacBook-Pro.local>
2015-04-27 19:33:42 -0700

    comment

---
+++ minimum_change_yen.rb
@@ -0,0 +1,51 @@
+coin = [500, 100, 50, 10, 5, 1]
+#c[0] = 500, c[1] = 100, ...
+c = [6]
+i = 0
+maisu = 0
+sum = 0
+ 
+print coin.size
+ 
+ 
+puts "input total yen"
+yen = gets.chomp.to_i
+ 
+puts "input the amount of each coin"
+print "500 > "
+c[0] = gets.chomp.to_i
+ 
+print "100 > "
+c[1] = gets.chomp.to_i
+ 
+print "50 > "
+c[2] = gets.chomp.to_i
+ 
+print "10 > "
+c[3] = gets.chomp.to_i
+ 
+print "5 > "
+c[4] = gets.chomp.to_i
+ 
+print "1 > "
+c[5] = gets.chomp.to_i
+ 
+ 
+while i < coin.size do
+  maisu = yen / coin[i]
+ 
+  if maisu > c[i]
+    maisu =  c[i]
+  end
+ 
+  yen = yen - (maisu * coin[i])
+  sum = sum + maisu
+  c[i] = maisu
+  i += 1
+end
+ 
+puts "total amount of coin >>> #{sum}"
+ 
+c.each do |coins|
+  puts coins
+end
